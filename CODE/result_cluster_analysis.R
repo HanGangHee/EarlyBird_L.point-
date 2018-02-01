9bc7bddb368e518c4652b176e5de6e343b5193ff
HanGangHee <tkdlaos21@ajou.ac.kr>
2018-02-01 21:08:25 +0900

    클러스터 결과 분석

--- CODE/result_cluster_analysis.R
+++ CODE/result_cluster_analysis.R
@@ -39,6 +39,72 @@ pie(slice2, labels = lbls2, main="cluser2", col = rainbow(length(lbls2)))
 pie(slice3, labels = lbls2, main="cluser3", col = rainbow(length(lbls2)))
 pie(slice4, labels = lbls2, main="cluser4", col = rainbow(length(lbls2)))
 
+#나이와 성별 비교
+
+cluster1 <- inner_join(cluster1, customer, by = c("ID" = "ID"))
+cluster2 <- inner_join(cluster2, customer, by = c("ID" = "ID"))
+cluster3 <- inner_join(cluster3, customer, by = c("ID" = "ID"))
+cluster4 <- inner_join(cluster4, customer, by = c("ID" = "ID"))
+
+cluster1.analysis <- cluster1 %>%
+  group_by(ID, AGE_PRD, GENDER) %>%
+  summarise(count_id = length(ID))
+cluster1.analysis <- as.data.frame(cluster1.analysis)
+
+
+cluster2.analysis <- cluster2 %>%
+  group_by(ID, AGE_PRD, GENDER) %>%
+  summarise(count_id = length(ID))
+
+cluster3.analysis <- cluster3 %>%
+  group_by(ID, AGE_PRD, GENDER) %>%
+  summarise(count_id = length(ID))
+
+cluster4.analysis <- cluster4 %>%
+  group_by(ID, AGE_PRD, GENDER) %>%
+  summarise(count_id = length(ID))
+
+cluster1.analysis <- as.data.frame(cluster1.analysis)
+cluster2.analysis <- as.data.frame(cluster2.analysis)
+cluster3.analysis <- as.data.frame(cluster3.analysis)
+cluster4.analysis <- as.data.frame(cluster4.analysis)
+
+str(cluster2.analysis)
+
+table(cluster1.analysis$AGE_PRD, cluster1.analysis$GENDER)
+table(cluster2.analysis$AGE_PRD, cluster2.analysis$GENDER)
+table(cluster3.analysis$AGE_PRD, cluster3.analysis$GENDER)
+table(cluster4.analysis$AGE_PRD, cluster4.analysis$GENDER)
+
+barplot(c(length(cluster1.analysis$ID),
+          length(cluster2.analysis$ID),
+          length(cluster3.analysis$ID),
+          length(cluster4.analysis$ID)),
+        names = c("cluster1", "cluster2", "cluster3", "cluster4"),
+        main = "각cluster별 고객의 수",
+        col = rainbow(4))
+
+
+barplot(c(mean(cluster1$BUY_AM),
+          mean(cluster2$BUY_AM),
+          mean(cluster3$BUY_AM),
+          mean(cluster4$BUY_AM)),
+        names = c("cluster1", "cluster2", "cluster3", "cluster4"),
+        main = "각cluster별 고객의 평균 소비량",
+        col = rainbow(8))
+
+barplot(c(mean(cluster1.analysis$count_id),
+          mean(cluster2.analysis$count_id),
+          mean(cluster3.analysis$count_id),
+          mean(cluster4.analysis$count_id)),
+        names = c("cluster1", "cluster2", "cluster3", "cluster4"),
+        main = "각cluster별 고객의 평균 이용 횟수",
+        col = rainbow(8))
+
+barplot(table(cluster1.analysis$AGE_PRD), main="Age of Cluster1")
+barplot(table(cluster2.analysis$AGE_PRD), main="Age of Cluster2")
+barplot(table(cluster3.analysis$AGE_PRD), main="Age of Cluster3")
+barplot(table(cluster4.analysis$AGE_PRD), main="Age of Cluster4")
 
 
 
