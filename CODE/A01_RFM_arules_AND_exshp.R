6a71795285efab1860a36e512f6d7b1b4e0caa33
HanGangHee <tkdlaos21@ajou.ac.kr>
2018-02-13 16:54:19 +0900

    함수로 변경

--- CODE/A01_RFM_arules_AND_exshp.R
+++ CODE/A01_RFM_arules_AND_exshp.R
@@ -2,55 +2,47 @@
 
 library(arulesViz)
 
-str(shpInfo.A01.rfm.data.rec1.freq1.monet1)
-shpInfo.A01.rfm.data.rec1.freq1.monet1.merge <- merge(total_shp,
-                                                      shpInfo.A01.rfm.data.rec1.freq1.monet1,
-                                                      by = "ID",
-                                                      all.x = T)
-shpInfo.A01.rfm.data.rec1.freq1.monet1.merge <- na.omit(shpInfo.A01.rfm.data.rec1.freq1.monet1.merge)
-str(shpInfo.A01.rfm.data.rec1.freq1.monet1.merge)
 
-shpInfo.A01.rfm.data.rec1.freq1.monet1.merge.temp <- shpInfo.A01.rfm.data.rec1.freq1.monet1.merge[, c("ID", "PD_M_NM")]
-shpInfo.A01.rfm.data.rec1.freq1.monet1.merge.temp$PD_M_NM <- as.factor(shpInfo.A01.rfm.data.rec1.freq1.monet1.merge.temp$PD_M_NM)
+# arules를 위한 transaction matrix 생성
+createTransaction <- function(rfmData){
+  rfmData <- merge(total_shp[,c("ID", "PD_M_NM")],
+                rfmData[,c(1,2)],
+                by = "ID",
+                all.x = T)
+  rfmData <- na.omit(rfmData)
+  rfmData <- rfmData[, c("ID","PD_M_NM")]
+  rfmData[, "PD_M_NM"] <- as.factor(rfmData[, "PD_M_NM"])
+  rfmData <-  as.data.frame.matrix(table(rfmData$ID, rfmData$PD_M_NM))
+  rfmData <- rfmData[rowSums(rfmData) > 0,]  
+  rfmData[rfmData > 1] <- 1 
+  rfmData <- as.(as.matrix(rfmData), "transactions")
+  return(rfmData)
+}
+
+#분석 예제
+shpA01.rfm.rec1.freq1.mona1.transaction <- createTransaction(shpA01.rfm.rec1.freq1.mona1)
+class(shpA01.rfm.rec1.freq1.mona1.transaction)
+summary(shpA01.rfm.rec1.freq1.mona1.transaction)
+inspect(shpA01.rfm.rec1.freq1.mona1.transaction[1:10])
+itemFrequency(shpA01.rfm.rec1.freq1.mona1.transaction, type = 'absolute')
 
 
-table(shpInfo.A01.rfm.data.rec1.freq1.monet1.merge.temp$ID, shpInfo.A01.rfm.data.rec1.freq1.monet1.merge.temp$PD_M_NM)
-shpInfo.A01.rfm.data.rec1.freq1.monet1.merge.temp <- as.data.frame.matrix(table(shpInfo.A01.rfm.data.rec1.freq1.monet1.merge.temp$ID, shpInfo.A01.rfm.data.rec1.freq1.monet1.merge.temp$PD_M_NM)
-)
 
-
-shpInfo.A01.rfm.data.rec1.freq1.monet1.merge.temp <- shpInfo.A01.rfm.data.rec1.freq1.monet1.merge.temp[rowSums(shpInfo.A01.rfm.data.rec1.freq1.monet1.merge.temp) > 0,]
-shpInfo.A01.rfm.data.rec1.freq1.monet1.merge.temp[shpInfo.A01.rfm.data.rec1.freq1.monet1.merge.temp > 1] <- 1
-shpInfo.A01.rfm.data.rec1.freq1.monet1.merge.temp <- as.matrix(shpInfo.A01.rfm.data.rec1.freq1.monet1.merge.temp)
-shpInfo.A01.rfm.data.rec1.freq1.monet1.merge.temp <- as(shpInfo.A01.rfm.data.rec1.freq1.monet1.merge.temp, "transactions")
-
-
-
-class(shpInfo.A01.rfm.data.rec1.freq1.monet1.merge.temp)
-summary(shpInfo.A01.rfm.data.rec1.freq1.monet1.merge.temp)
-inspect(shpInfo.A01.rfm.data.rec1.freq1.monet1.merge.temp[1:10])
-itemFrequency(shpInfo.A01.rfm.data.rec1.freq1.monet1.merge.temp, type = 'absolute')
+#exShpInfo
+rfmWithExShp <- function(rfmData){
+  rfmData <- na.omit(merge(exShpInfo[, c("ID", "BIZ_UNIT")],
+                   rfmData[, c(1,2)],
+                   by = "ID",
+                   all.x = T))
+  rfmData <- rfmData[, c("ID", "BIZ_UNIT")]
+  return(rfmData)
+}
 
 # margin 문제 해결
+#(B01:호텔, B02:여행사, B03:면세점,C01:영화관, C02:테마파크, C03:야구관람, D01:패스트푸드, D02:패밀리레스토랑, D03:카페)
+
 par("mar")
 par(mar = c(1, 1, 1, 1))
 
-itemFrequencyPlot(shpInfo.A01.rfm.data.rec1.freq1.monet1.merge.temp, topN = 10)
-
-
-
-
-shpInfo.A01.rfm.data.rec1.freq1.monet1.exshp <- merge(exShpInfo, 
-                                                      shpInfo.A01.rfm.data.rec1.freq1.monet1,
-                                                      by = "ID",
-                                                      all.x = T)
-shpInfo.A01.rfm.data.rec1.freq1.monet1.exshp <- na.omit(shpInfo.A01.rfm.data.rec1.freq1.monet1.exshp)
-str(shpInfo.A01.rfm.data.rec1.freq1.monet1.exshp)
-shpInfo.A01.rfm.data.rec1.freq1.monet1.exshp <- shpInfo.A01.rfm.data.rec1.freq1.monet1.exshp[, c(1,2)]
-table(shpInfo.A01.rfm.data.rec1.freq1.monet1.exshp$BIZ_UNIT)
-barplot(table(shpInfo.A01.rfm.data.rec1.freq1.monet1.exshp$BIZ_UNIT),
-        names = c("B01", "B02", "B03", "C01", "C02", "c03", "D01", "D02", "D03"))
-"""
-(B01:호텔, B02:여행사, B03:면세점,
-C01:영화관, C02:테마파크, C03:야구관람, D01:패스트푸드, D02:패밀리레스토랑, D03:카페)
-"""
+#table()
+#barplot(,names = c("B01", "B02", "B03", "C01", "C02", "c03", "D01", "D02", "D03"))
--- CODE/RFM.R
+++ CODE/RFM.R
@@ -1,79 +1,125 @@
 #load("C:/Users/gksrkdgml/EarlyBird_L.point-/DATA/total_shp.RDa")
 #load("C:/Users/gksrkdgml/EarlyBird_L.point-/DATA/BEST_SHP.RDa")
+
 library(dplyr)
 #install.packages("devtools")
 library(easyRFM)
-devtools::install_github("hoxo-m/easyRFM")
+#devtools::install_github("hoxo-m/easyRFM")
+
 #(A01:백화점, A02:대형마트, A03:슈퍼마켓, A04:편의점, A05:드러그스토어)
+
+
+shpInfo <- total_shp
 str(shpInfo)
+#shpInfo 전처리
 shpInfo$ID <- as.factor(shpInfo$ID)
 shpInfo$DE_DT <- as.character(shpInfo$DE_DT)
 shpInfo$DE_DT <- as.Date(shpInfo$DE_DT, format = "%Y%m%d")
-shpInfo.A01 <- shpInfo[shpInfo$BIZ_UNIT == 'A01', ]
-shpInfo.A02 <- shpInfo[shpInfo$BIZ_UNIT == 'A02', ]
-shpInfo.A03 <- shpInfo[shpInfo$BIZ_UNIT == 'A03', ]
-shpInfo.A04 <- shpInfo[shpInfo$BIZ_UNIT == 'A04', ]
-shpInfo.A05 <- shpInfo[shpInfo$BIZ_UNIT == 'A05', ]
 
-shpInfo.A01.rfm <- shpInfo.A01[, c('ID', 'DE_DT', 'BUY_AM')] 
-shpInfo.A02.rfm <- shpInfo.A02[, c('ID', 'DE_DT', 'BUY_AM')] 
-shpInfo.A03.rfm <- shpInfo.A03[, c('ID', 'DE_DT', 'BUY_AM')] 
-shpInfo.A04.rfm <- shpInfo.A04[, c('ID', 'DE_DT', 'BUY_AM')] 
-shpInfo.A05.rfm <- shpInfo.A05[, c('ID', 'DE_DT', 'BUY_AM')] 
-
-length(unique(shpInfo.A01.rfm$ID))
-length(unique(shpInfo.A02.rfm$ID))
-length(unique(shpInfo.A03.rfm$ID))
-length(unique(shpInfo.A04.rfm$ID))
-length(unique(shpInfo.A05.rfm$ID))
+# 업종별로 데이터 분리
+shpInfo.A01 <- shpInfo[shpInfo$BIZ_UNIT == 'A01', c('ID', 'DE_DT', 'BUY_AM')]
+shpInfo.A02 <- shpInfo[shpInfo$BIZ_UNIT == 'A02', c('ID', 'DE_DT', 'BUY_AM')]
+shpInfo.A03 <- shpInfo[shpInfo$BIZ_UNIT == 'A03', c('ID', 'DE_DT', 'BUY_AM')]
+shpInfo.A04 <- shpInfo[shpInfo$BIZ_UNIT == 'A04', c('ID', 'DE_DT', 'BUY_AM')]
+shpInfo.A05 <- shpInfo[shpInfo$BIZ_UNIT == 'A05', c('ID', 'DE_DT', 'BUY_AM')]
 
 
-#5점?  3점? 
-shpInfo.A01.rfm <- shpInfo.A01 %>%
-                        group_by(ID) %>%
-                          summarise(recency = max(as.numeric(DE_DT)) - as.numeric(as.Date("2014-12-29" , format = "%Y-%m-%d")),
-                                    frequenci = length(ID), monitery = sum(BUY_AM)/length(ID))
-summary(shpInfo.A01.rfm)
+# 업종별로 이용하는 고객의 수 
+length(unique(shpInfo.A01$ID))
+length(unique(shpInfo.A02$ID))
+length(unique(shpInfo.A03$ID))
+length(unique(shpInfo.A04$ID))
+length(unique(shpInfo.A05$ID))
+
+
+shpA01.rfm <- rfm_auto(shpInfo.A01, payment = "BUY_AM", date = "DE_DT", id = "ID", breaks = list(r=3, f=3,m=3))
+shpA02.rfm <- rfm_auto(shpInfo.A02, payment = "BUY_AM", date = "DE_DT", id = "ID", breaks = list(r=3, f=3,m=3))
+shpA03.rfm <- rfm_auto(shpInfo.A03, payment = "BUY_AM", date = "DE_DT", id = "ID", breaks = list(r=3, f=3,m=3))
+shpA04.rfm <- rfm_auto(shpInfo.A04, payment = "BUY_AM", date = "DE_DT", id = "ID", breaks = list(r=3, f=3,m=3))
+shpA05.rfm <- rfm_auto(shpInfo.A05, payment = "BUY_AM", date = "DE_DT", id = "ID", breaks = list(r=3, f=3,m=3))
 
 
 
-shpInfo.A02.rfm <- shpInfo.A02 %>%
-  group_by(ID) %>%
-  summarise(recency = max(as.numeric(DE_DT)) - as.numeric(as.Date("2014-12-29" , format = "%Y-%m-%d")),
-            frequenci = length(ID), monitery = sum(BUY_AM)/length(ID))
-summary(shpInfo.A02.rfm)
+A01 <- "shpA01.rfm"
+A02 <- "shpA02.rfm"
+A03 <- "shpA03.rfm"
+A04 <- "shpA04.rfm"
+A05 <- "shpA05.rfm"
 
-shpInfo.A03.rfm <- shpInfo.A03 %>%
-  group_by(ID) %>%
-  summarise(recency = max(as.numeric(DE_DT)) - as.numeric(as.Date("2014-12-29" , format = "%Y-%m-%d")),
-            frequenci = length(ID), monitery = sum(BUY_AM)/length(ID))
-summary(shpInfo.A02.rfm)
+# A01 27개로 segment
+for(i in 1:3){
+  name = A01
+  for(j in 1:3){
+    for(k in 1:3){
+      name <- paste(name, paste(".rec",i, sep = ""), sep = "")
+      name<- paste(name, paste(".freq",j, sep = ""), sep = "")
+      name <- paste(name, paste(".mone",k, sep = ""), sep = "")
+      assign(name, shpA01.rfm$rfm[shpA01.rfm$rfm$RecencyClass == i &
+                                         shpA01.rfm$rfm$FrequencyClass == j &
+                                           shpA01.rfm$rfm$MonetaryClass == k, ])
+    }
+  }
+}
 
+# A02 27개로 segment
+for(i in 1:3){
+  name = A02
+  for(j in 1:3){
+    for(k in 1:3){
+      name <- paste(name, paste(".rec",i, sep = ""), sep = "")
+      name<- paste(name, paste(".freq",j, sep = ""), sep = "")
+      name <- paste(name, paste(".mone",k, sep = ""), sep = "")
+      assign(name, shpA02.rfm$rfm[shpA02.rfm$rfm$RecencyClass == i &
+                                         shpA02.rfm$rfm$FrequencyClass == j &
+                                         shpA02.rfm$rfm$MonetaryClass == k, ])
+    }
+  }
+}
 
-shpInfo.A04.rfm <- shpInfo.A04 %>%
-  group_by(ID) %>%
-  summarise(recency = max(as.numeric(DE_DT)) - as.numeric(as.Date("2014-12-29" , format = "%Y-%m-%d")),
-            frequenci = length(ID), monitery = sum(BUY_AM)/length(ID))
-summary(shpInfo.A04.rfm)
+# A03 27개로 segment
+for(i in 1:3){
+  name = A03
+  for(j in 1:3){
+    for(k in 1:3){
+      name <- paste(name, paste(".rec",i, sep = ""), sep = "")
+      name<- paste(name, paste(".freq",j, sep = ""), sep = "")
+      name <- paste(name, paste(".mone",k, sep = ""), sep = "")
+      assign(name, shpA03.rfm$rfm[shpA03.rfm$rfm$RecencyClass == i &
+                                         shpA03.rfm$rfm$FrequencyClass == j &
+                                         shpA03.rfm$rfm$MonetaryClass == k, ])
+    }
+  }
+}
 
+# A04 27개로 segment
+for(i in 1:3){
+  name = A04
+  for(j in 1:3){
+    for(k in 1:3){
+      name <- paste(name, paste(".rec",i, sep = ""), sep = "")
+      name<- paste(name, paste(".freq",j, sep = ""), sep = "")
+      name <- paste(name, paste(".mone",k, sep = ""), sep = "")
+      assign(name, shpA04.rfm$rfm[shpA04.rfm$rfm$RecencyClass == i &
+                                         shpA04.rfm$rfm$FrequencyClass == j &
+                                         shpA04.rfm$rfm$MonetaryClass == k, ])
+    }
+  }
+}
 
-shpInfo.A05.rfm <- shpInfo.A05 %>%
-  group_by(ID) %>%
-  summarise(recency = max(as.numeric(DE_DT)) - as.numeric(as.Date("2014-12-29" , format = "%Y-%m-%d")),
-            frequenci = length(ID), monitery = sum(BUY_AM)/length(ID))
-summary(shpInfo.A05.rfm)
-
-###
-
-
-shpInfo.A01.rfm <- rfm_auto(shpInfo.A01, payment = "BUY_AM", date = "DE_DT", id = "ID", breaks = list(r=3, f=3,m=3))
-str(shpInfo.A01.rfm$rfm)
-shpInfo.A01.rfm$breaks
-shpInfo.A01.rfm$classes
-shpInfo.A01.rfm$get_table()
-table(shpInfo.A01.rfm$get_sliced_rfm()$RecencyClass)
-
-
+# A05 27개로 segment
+for(i in 1:3){
+  name = A05
+  for(j in 1:3){
+    for(k in 1:3){
+      name <- paste(name, paste(".rec",i, sep = ""), sep = "")
+      name<- paste(name, paste(".freq",j, sep = ""), sep = "")
+      name <- paste(name, paste(".mone",k, sep = ""), sep = "")
+      assign(name, shpInfo.A05.rfm$rfm[shpA05.rfm$rfm$RecencyClass == i &
+                                         shpA05.rfm$rfm$FrequencyClass == j &
+                                         shpA05.rfm$rfm$MonetaryClass == k, ])
+    }
+  }
+}
 
 
 
